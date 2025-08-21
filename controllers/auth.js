32c758fc5221d1b24a7cbd68131fffa0156e75b9
kaberege <kabgnestor@gmail.com>
2025-08-21 20:48:58 +0200

    fix: fixed authentication middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,46 +2,56 @@ const userModel = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.userAuth = (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    bcrypt.hash(password, 12)
-        .then(hashedPW => {
-            const newUser = new userModel({
-                name: name,
-                email: email,
-                password: hashedPW
-            });
-            console.log(newUser);
-            return newUser.save();
-        })
-        .then(user => {
-            console.log("My result.")
-            res.status(201).json({ message: "User created successfully.", userId: user._id })
-        })
-        .catch(error => console.log(error));
-}
+exports.userAuth = async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
 
-exports.userLogin = (req, res) => {
-    console.log("called");
-    const email = req.body.email;
-    const password = req.body.password;
-    let loaded;
-    userModel.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                console.log("User with that email not found!")
-            }
-            loaded = user;
-            return bcrypt.compare(password, user.password);
-        })
-        .then(isEqual => {
-            if (!isEqual) {
-                console.log("Password did not match!")
-            }
-            const token = jwt.sign({ email: loaded.email, userId: loaded._id.toString() }, "secrettoken", { expiresIn: "1h" });
-            res.status(200).json({ token: token, userId: loaded._id.toString() })
-        })
-        .catch(error => console.log("Request failed.", error));
-}
+  try {
+    const hashedPW = await bcrypt.hash(password, 12);
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPW,
+    });
+
+    const user = await newUser.save();
+
+    res
+      .status(201)
+      .json({ message: "User created successfully.", userId: user._id });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.userLogin = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await userModel.findOne({ email: email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User with that email not found!" });
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+
+    if (!isEqual) {
+      return res.status(400).json({ message: "Password did not match!" });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, userId: user._id.toString() },
+      "secrettoken",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token: token, userId: user._id.toString() });
+  } catch (error) {
+    res.status(500).json({ message: "Request failed", error: error.message });
+  }
+};
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,20 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const authHeader = req.get("Authorization");
-    if (!authHeader) {
-        console.log("Authorisation header is missing!");
-    }
-    const token = authHeader.split(" ")[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, "secrettoken");
-    } catch (error) {
-        console.log(error);
-    }
-    if (!decodedToken) {
-        console.log("Not authenticated.");
-    }
-    req.userId = decodedToken.userId;
-    next();
-}
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    return res
+      .status(404)
+      .json({ message: "Authorisation header is missing!" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, "secrettoken");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+  if (!decodedToken) {
+    return res.status(400).json({ message: "Token is not valid" });
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
